d306113084ffde4e070aab1be3b3558b8be16cdd
Felamar <114794450+Felamar@users.noreply.github.com>
2024-03-07 15:29:58 -0600

    Refactor XOR neural network training and prediction

--- main.m
+++ main.m
@@ -1,13 +1,16 @@
-cases = [ % XOR
-        1    1    1e-5; 
-        1    1e-5 1; 
-        1e-5 1    1;
-        1e-5 1e-5 1e-5
+clc
+cases = [      % XOR
+        1 1 0; % Case 1
+        1 0 1; % Case 2
+        0 1 1; % Case 3
+        0 0 0  % Case 4
     ];
-
-i = input("case no:");
+rng(69) % seed for random number generator for reproducibility
+i = input("Case no: ");
+fprintf('Training the neural network with the case: [%d %d]\nExpected value: %d\n\n', cases(i, :));
 nn = neural_network();  
 nn = nn.BackPropagation(cases(i, :));
-
 prediction = nn.Predict(cases(i, 1:2));
-disp(prediction);
+
+fprintf('\nPrediction for the given case: %d\n', prediction);
+
--- neural_network.m
+++ neural_network.m
@@ -1,154 +1,122 @@
 classdef neural_network
     properties (Access = public)
-        % number of layers in the network (including the input layer)
-        n_layers
-        % 1D array of layer sizes
-        % where the index represents the layer
-        szs 
-        % 3D array of weight matrices for all layers
-        % where the row index represents the neuron in the previous layer
-        % and the column index represents the neuron in the current layer
-        weights 
-        % 1D array of activation vectors for all layers
-        % where the index represents the bias of the layer
-        bias
-        % 2D array of activation vectors for all layers
-        % where the row index represents the neuron in the layer
-        % and the column index represents the layer
-        activations
-        % 2D array of z value vectors for all layers
-        % where the row index represents the neuron in the layer
-        % and the column index represents the layer
-        zs
+        % Neural Network properties
+        n_layers % Number of layers in the network
+        szs      % Sizes of the layers
+        bias     % Bias vector
         
+        % Weights for each layer
+        w_h1 
+        w_h2
+        w_o
+        % Activations of the layers
+        a_h1
+        a_h2
+        a_o
+        % z values of the layers
+        z_h1
+        z_h2
+        z_o
     end
 
     methods
-        % inputs = vector de  2 valores
-        % hid_size = cantidad de neuronas en la capa oculta
-        % bias = vector de 3 valores
-        % out_size = cantidad de neuronas en la capa de salida
         function obj = neural_network()
-            n_inputs     = 2;      % número de entradas (2 bits)
-            hid_sz       = [2, 3]; % número de neuronas en las capas ocultas
-            out_sz       = 1;      % número de neuronas en la capa de salida
-            obj.n_layers = 4;      % número de capas (incluyendo la de entrada)
-            obj.szs      = [n_inputs, hid_sz(1), hid_sz(2), out_sz]; % vector de tamaños de capas
+            n_inputs     = 2;      % Input layer size
+            hid_sz       = [2, 3]; % Hidden layers sizes
+            out_sz       = 1;      % Output layer size
+            obj.n_layers = 4;      % Layers in the network
+            obj.szs      = [n_inputs, hid_sz(1), hid_sz(2), out_sz]; % Layer sizes
+            obj.bias     = rand(1, 3); % bias vector
 
-            obj.weights     = [];                               % matriz de pesos
-            % obj.bias        = rand(1, 3);         % vector de bias
-            obj.bias        = [1 1 1 ];         % vector de bias
-            obj.activations = zeros(max(obj.szs), obj.n_layers-1);  % matriz de activaciones
-            obj.zs          = zeros(max(obj.szs), obj.n_layers-1);  % matriz de activaciones
+            % Weight values for each layer
+            obj.w_h1 = rand(2, 2) - 0.5;
+            obj.w_h2 = rand(2, 3) - 0.5;
+            obj.w_o  = rand(3, 1) - 0.5;
 
-            % Inicialización de la matriz de pesos
-            for matrix_i = 2:obj.n_layers
-                currentl_sz  = obj.szs(matrix_i);
-                previousl_sz = obj.szs(matrix_i-1);
-                for row = 1:previousl_sz
-                    for col = 1:currentl_sz
-                        obj.weights(row, col, matrix_i-1) = rand() - 0.5;
-                    end
-                end
-            end
+            % z values of the layers
+            obj.z_h1 = zeros(2);
+            obj.z_h2 = zeros(3);
+            obj.z_o  = zeros(1); 
 
+            % Activations of the layers
+            obj.a_h1 = zeros(2); 
+            obj.a_h2 = zeros(3); 
+            obj.a_o  = zeros(1); 
+            
         end
 
         function obj = ForwardPropagation(obj, inputs)
-            % inputs is a 1D array of input values (2 bits)
-            inputs = [inputs 0];
-            n_matrix = obj.n_layers - 1;
-            for matrix_i = 1:n_matrix
-                currentl_sz = obj.szs(matrix_i+1);
-                p_layer = matrix_i;
-                for neuron_i = 1:currentl_sz
-                    if matrix_i == 1
-                        z = inputs * obj.weights(:, neuron_i, matrix_i) + obj.bias(matrix_i);
-                    else
-                        z = obj.activations(:, p_layer)' * obj.weights(:, neuron_i, matrix_i) + obj.bias(matrix_i);
-                    end
-                    obj.zs(neuron_i,matrix_i) = z;
-                    obj.activations(neuron_i, matrix_i) = Sigmoid(z);
-                end
-            end
+            % Activation * Weights + Bias
+            obj.z_h1 = inputs   * obj.w_h1 + obj.bias(1); % 1x2 * 2x2 
+            obj.a_h1 = Sigmoid(obj.z_h1);
+
+            obj.z_h2 = obj.a_h1 * obj.w_h2 + obj.bias(2); % 1x2 * 2x3
+            obj.a_h2 = Sigmoid(obj.z_h2);
+
+            obj.z_o  = obj.a_h2 * obj.w_o  + obj.bias(3); % 1x3 * 3x1
+            obj.a_o  = Sigmoid(obj.z_o);
         end
 
         function obj = BackPropagation(obj, cases)
-            % input is a 1D array of input values (2 bits)
-            % expected is a value of the expected output (XOR gate)
-            % input = [1, 1];
-            % expected = 0;
             n_iterations = 1e5;
-            learning_rate = 0.01;
-            o = 3;
-            h2 = 2;
-            h1 = 1;
-            error_2  = 10;
-            
-            while n_iterations > 0
-                av_out_w_costs  = zeros(3, 3);
-                av_h2_w_costs   = zeros(3, 3);
-                av_h1_w_costs   = zeros(3, 3);
+            learning_rate = 0.005;
+            error_2  = zeros(1, n_iterations);
+            counter  = 1;
+            inputs   = cases(1:2);
+            expected = cases(3);
+
+            while counter <= n_iterations
                 
+                % Forward Propagation
                 % -----------------------------------------------------------------------------------------
-                % for case_i = 1:4
-                %     inputs   = cases(case_i, 1:2);
-                %     expected = cases(case_i, 3);
-                %     obj      = obj.ForwardPropagation(inputs);
-                %     error    = obj.activations(1, o) - expected;
-                %     error_2  = error^2;
-                    
-                %     out_w_costs = obj.activations( : , h2) .* SigmoidPrime(obj.zs(1,o))  * (error);
-                %     delta_o     = obj.weights(:, 1, o)     .* SigmoidPrime(obj.zs(1,o))  * (error);
-                    
-                %     h2_w_costs  = obj.activations(1:2, h1) * SigmoidPrime(obj.zs(:,2))' .* delta_o';
-                %     delta_h2    = obj.weights(1:2, : , h2) * (SigmoidPrime(obj.zs(:,h2)) .* delta_o);
+                obj   = obj.ForwardPropagation(inputs);
+                error = obj.a_o - expected;
+                error_2(1, counter) = error^2 / 2;
 
-                %     h1_w_costs  = inputs' * SigmoidPrime(obj.zs(1:2,h1))' .* delta_h2';
-
-                %     av_out_w_costs(: ,   1) = av_out_w_costs( : ,   1) + out_w_costs;
-                %     av_h2_w_costs(1:2,  : ) = av_h2_w_costs( 1:2,  : ) + h2_w_costs;
-                %     av_h1_w_costs(1:2, 1:2) = av_h1_w_costs(1:2, 1:2)  + h1_w_costs;
-
-                % end
-                % av_out_w_costs = av_out_w_costs ./ 4;
-                % av_h2_w_costs  = av_h2_w_costs  ./ 4;
-                % av_h1_w_costs  = av_h1_w_costs  ./ 4;
-                
+                % Gradient Descent
                 % -----------------------------------------------------------------------------------------
 
-                inputs   = cases(1:2);
-                expected = cases(3);
-                obj      = obj.ForwardPropagation(inputs);
+                % Activation * SigmoidPrime * Error
+                out_w_costs = obj.a_h2 .* SigmoidPrime(obj.z_o) .* (error);
+                delta_o     = obj.w_o  .* SigmoidPrime(obj.z_o) * (error);
 
-                error    = obj.activations(1, o) - expected;
-                error_2  = error^2;
+                % Activation * SigmoidPrime * Sum(phi_o * w_o)
+                h2_w_costs  = obj.a_h1' * SigmoidPrime(obj.z_h2) .* delta_o';
+                delta_h2    = obj.w_h2  * (SigmoidPrime(obj.z_h2) .* delta_o')';
 
-                out_w_costs = obj.activations( : , h2) .* SigmoidPrime(obj.zs(1,o))  * (error);
-                delta_o     = obj.weights(:, 1, o)     .* SigmoidPrime(obj.zs(1,o))  * (error);
+                % Activation * SigmoidPrime * Sum(phi_h2 * w_h2)
+                h1_w_costs  = inputs' * SigmoidPrime(obj.z_h1) .* delta_h2';
                 
-                h2_w_costs  = obj.activations(1:2, h1) * SigmoidPrime(obj.zs(:,2))'  .* delta_o';
-                delta_h2    = obj.weights(1:2, : , h2) * (SigmoidPrime(obj.zs(:,h2)) .* delta_o);
-
-                h1_w_costs  = inputs' * SigmoidPrime(obj.zs(1:2,h1))' .* delta_h2';
-
-                av_out_w_costs(: ,   1) = av_out_w_costs( : ,   1) + out_w_costs;
-                av_h2_w_costs(1:2,  : ) = av_h2_w_costs( 1:2,  : ) + h2_w_costs;
-                av_h1_w_costs(1:2, 1:2) = av_h1_w_costs(1:2, 1:2)  + h1_w_costs;
-                
+                % Update weights
                 % -----------------------------------------------------------------------------------------
+                obj.w_o(:, :)  = obj.w_o( :, :) - learning_rate .* out_w_costs';
+                obj.w_h2(:, :) = obj.w_h2(:, :) - learning_rate .* h2_w_costs;
+                obj.w_h1(:, :) = obj.w_h1(:, :) - learning_rate .* h1_w_costs';
 
-                obj.weights( : ,   1, o)  = obj.weights( : ,   1, o)  - learning_rate * av_out_w_costs( : ,  1);
-                obj.weights(1:2,  : , h2) = obj.weights(1:2,  : , h2) - learning_rate * av_h2_w_costs(1:2,  : );
-                obj.weights(1:2, 1:2, h1) = obj.weights(1:2, 1:2, h1) - learning_rate * av_h1_w_costs(1:2, 1:2);
-                n_iterations = n_iterations - 1;
+                counter = counter + 1;
             end
-            
+            % Plot error
+            % -----------------------------------------------------------------------------------------
+            figure;
+            plot(error_2, 'r');
+            title('Error across epochs');
+            txt = ['Case: [', num2str(cases(1)), ' ', num2str(cases(2)), '] Expected: ', num2str(floor(cases(3)))];
+            subtitle(txt);
+            legend('Error');
+            xlabel('Epochs');
+            ylabel('Error');
+            grid on
+            set(gcf, 'Position', [100, 100, 1500, 750]);
+            xlim([-1e3, n_iterations+1e3]);
+            ylim([-0.01, 0.25]);
+            disp(['Final error: ', num2str(error_2(end))])
         end
 
         function output = Predict(obj, inputs)
             obj = obj.ForwardPropagation(inputs);
-            output = obj.activations(1, obj.n_layers-1);
+            output = obj.a_o;
         end
     end
+    % floor division
+
 end
